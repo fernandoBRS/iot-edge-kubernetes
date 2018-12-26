9919995af24753c156c4e434dd146293070a7814
Fernando de Oliveira <fedeoliv@microsoft.com>
2018-12-26 22:06:49 +0000

    Script to deploy the Edge Helm chart

---
+++ scripts/iot-hub/deploy-edge-helm.sh
@@ -0,0 +1,12 @@
+# Variables
+keyvault_name="contosokvaultdev"
+device_prefix="gateway"
+
+# Get the device connection string on Key Vault
+device_connection_string=$(az keyvault secret show --vault-name $keyvault_name --name $device_prefix-001 --query value -o tsv)
+
+# Deploy the Edge helm chart
+helm install \
+    --name edgy \
+    --set "deviceConnectionString=$device_connection_string" \
+    edgek8s/edge-kubernetes
