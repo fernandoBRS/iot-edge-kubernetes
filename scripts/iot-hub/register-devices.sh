34c0f1035724f72d6072afac7016f0bcc62b400a
Fernando de Oliveira <fedeoliv@microsoft.com>
2018-12-31 18:55:40 +0000

    Deploy a single edge device gateway

--- scripts/iot-hub/register-devices.sh
+++ scripts/iot-hub/register-devices.sh
@@ -1,17 +1,12 @@
 # Variables
-resource_group_name="rg-contoso-dev"
-keyvault_name="contosokvaultdev"
-iothub_name="contosohubdev"
+resource_group_name="your_resource_group_name"
+keyvault_name="your_keyvault_name"
+iothub_name="your_iothub_name"
 device_prefix="gateway"
 
 # Create two gateway devices
 az iot hub device-identity create \
     --device-id $device_prefix-001 \
-    --hub-name $iothub_name \
-    --edge-enabled
-
-az iot hub device-identity create \
-    --device-id $device_prefix-002 \
     --hub-name $iothub_name \
     --edge-enabled
 
@@ -27,13 +22,5 @@ az keyvault secret set \
     --name $device_prefix-001 \
     --value $(az iot hub device-identity show-connection-string \
         --device-id $device_prefix-001 \
-        --hub-name $iothub_name \
-        --query cs -o tsv)
-
-az keyvault secret set \
-    --vault-name $keyvault_name \
-    --name $device_prefix-002 \
-    --value $(az iot hub device-identity show-connection-string \
-        --device-id $device_prefix-002 \
         --hub-name $iothub_name \
         --query cs -o tsv)
