82cfa6914d1203c552280704e98d7ce4b4590fcd
Fernando de Oliveira <fedeoliv@microsoft.com>
2019-03-10 21:05:24 +0000

    Edge device name updated

--- scripts/iot-hub/register-devices.sh
+++ scripts/iot-hub/register-devices.sh
@@ -2,11 +2,11 @@
 resource_group_name="your_resource_group_name"
 keyvault_name="your_keyvault_name"
 iothub_name="your_iothub_name"
-device_prefix="gateway"
+device_name="edge-gateway"
 
 # Create two gateway devices
 az iot hub device-identity create \
-    --device-id $device_prefix-001 \
+    --device-id $device_name \
     --hub-name $iothub_name \
     --edge-enabled
 
@@ -16,11 +16,11 @@ az provider register -n Microsoft.KeyVault
 # Create a Key Vault
 az keyvault create --name $keyvault_name --resource-group $resource_group_name
 
-# Store connection strings from edge devices in Key Vault
+# Store the edge device connection string in Key Vault
 az keyvault secret set \
     --vault-name $keyvault_name \
     --name $device_prefix-001 \
     --value $(az iot hub device-identity show-connection-string \
-        --device-id $device_prefix-001 \
+        --device-id $device_name \
         --hub-name $iothub_name \
         --query cs -o tsv)
